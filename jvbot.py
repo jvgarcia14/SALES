f4886cedf6317b162e61b307c6f2a69090e00490
jvgarcia14 <60375104+jvgarcia14@users.noreply.github.com>
2026-01-17 17:13:25 +0800

    Update jvbot.py

--- jvbot.py
+++ jvbot.py
@@ -7,7 +7,9 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Messa
 
 # ================= CONFIG =================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ALLOWED_USERS = [5513230302, 6884192394, 6693305516]
+ALLOWED_USERS = [5513230302, 6884192394, 6693305516, 1816005650, 7162861125
+
+1816005650]
 # =========================================
 
 # Per-group state
@@ -145,4 +147,5 @@ if __name__ == "__main__":
 
     print("🤖 Auto-confirm sales bot running...")
     app.run_polling()
+
 
